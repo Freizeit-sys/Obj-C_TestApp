3de7b63a2843a72c7f4fd04f81c3451846d116e4
Freizeit-sys <work16087y0rha@gmail.com>
2022-07-05 00:21:26 +0900

    textView auto height

--- ObjCApp/AccViewController.m
+++ ObjCApp/AccViewController.m
@@ -7,11 +7,9 @@
 
 #import "AccViewController.h"
 
-@interface AccViewController ()
+@interface AccViewController ()<UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *menuViewHeightAnchor;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *contentViewHeightAnchor;
-
-
 
 
 @property (nonatomic) NSLayoutConstraint* textView1HeightAnchor;
@@ -44,13 +42,22 @@
     [super viewDidAppear:animated];
 }
 
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+}
+
 -(IBAction)handleTap:(id)sender {
     NSLog(@"aaa");
     switch ([sender tag]) {
         case 0:
         {
-            _menuViewHeightAnchor.constant = _menuViewHeightAnchor.constant == 52 ? 124 : 52;
-            _contentView1HeightAnchor.constant = _contentView1HeightAnchor == 0 ? 72 : 0;
+            //CGFloat height = [_textView1 sizeThatFits:CGSizeMake(_textView1.bounds.size.width, MAXFLOAT)].height;
+            //[_textView1 sizeToFit];
+            CGFloat height = [_textView1 systemLayoutSizeFittingSize:CGSizeMake(_textView1.frame.size.width, MAXFLOAT)].height + 10;
+            NSLog(@"%.1f", height);
+
+            _menuViewHeightAnchor.constant = _menuViewHeightAnchor.constant == 52 ? 52 + height : 52;
+            _contentViewHeightAnchor.constant = _contentViewHeightAnchor == 0 ? height : 0;
 //            _menuView1HeightAnchor.constant = _menuView1HeightAnchor.constant == 52 ? 124 : 52;
 //            _contentView1HeightAnchor.constant = _contentView1HeightAnchor.constant == 0 ? 72 : 0;
 //            _textView1HeightAnchor.constant = _textView1HeightAnchor.constant == 0 ? 72 : 0;
